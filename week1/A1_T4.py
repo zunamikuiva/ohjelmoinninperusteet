abb11108a7f11a7c6eb716ad818cc5548dc2dbd4
Nina Hinkkanen <nina.hinkkanen@hotmail.com>
2025-09-14 11:53:28 +0300

    Tehtävä 4 kirjoitettu

---
+++ week1/A1_T4.py
@@ -0,0 +1,19 @@
+#A1_T4 Printing and math operations 
+
+
+# Luodaan muuttujat
+Num1 = 47
+Num2 = 102
+
+
+# Summa ja erotus
+Sum = Num1 + Num2
+Diff = Num2 - Num1
+
+# Kerrotaan summa ja erotus
+Product = Sum * Diff
+
+print(f"{Num1} + {Num2} = {Sum}")
+print(f"{Num2} - {Num1} = {Diff}")
+print(f"{Sum} * {Diff} = {Product}")
+print(f"({Num1} + {Num2}) * ({Num2} - {Num1}) = {Product}")
