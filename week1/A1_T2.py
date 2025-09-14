0b1d542ff80f580092ec7d386823fe838674616f
Nina Hinkkanen <nina.hinkkanen@hotmail.com>
2025-09-14 11:09:58 +0300

    Tehtävä 2 kirjoitettu

---
+++ week1/A1_T2.py
@@ -0,0 +1,16 @@
+#A1_T2 Print variables 
+
+
+Name1 = "John"
+
+print(Name1)
+
+Name2 = "Harry"
+
+print(Name2)
+
+print(Name1 + " " + Name2)
+
+print(Name1 + " is eating ice cream with " + Name2)
+
+print(Name1 + " and " + Name2 + " are friends")
