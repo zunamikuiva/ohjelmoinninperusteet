9df174ae03d1aa410ba23993c7b03d1cff9f6b00
Nina Hinkkanen <nina.hinkkanen@hotmail.com>
2025-09-18 12:39:57 +0300

    Lisätty tehtävä A2_T1.py

--- week2/A2_T1.py
+++ week2/A2_T1.py
@@ -1,1 +1,11 @@
 #A2_T1 Basic program structure 
+
+
+print("Program starting.")
+name = input("What is your name: ")
+num1 = float(input("Enter a floating point number: "))
+num2 = float(input("Enter second floating point number: "))
+print(f"{name} you gave numbers {num1} and {num2}")
+sum = num1 * num2
+print(f"Multiplying first and second number will result in product {sum}")
+print("Program ending.")
