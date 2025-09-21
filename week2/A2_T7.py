45f10921907983cd6dad2763c4d6b3a101c913ba
Nina Hinkkanen <nina.hinkkanen@hotmail.com>
2025-09-21 12:49:34 +0300

    Lisätty 7 tehtävä

---
+++ week2/A2_T7.py
@@ -0,0 +1,7 @@
+# A2_T7 Fahrenheit to Celcius  
+
+print("Program starting.")
+fahr = float(input("Insert fahrenheits: "))
+celc = (fahr - 32) / 1.8
+print(f"{fahr}°F is {celc}°C")
+print("Program ending.")
